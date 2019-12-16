0ac00e7f5d41192e40619980162d9dd63d910cc7
loaki <jules.feuillatre@gmail.com>
2019-12-16 17:39:43 +0100

    z

--- test.c
+++ test.c
@@ -10,31 +10,28 @@ typedef struct    data_s
     void        *win_ptr;
 	void		*img_ptr;
 	void		*addr_ptr;
+
+	int			bpp;
+	int			size_l;
+	int			endiant;
 }                 data_t;
 
 int main(void)
 {
     data_t        *data;
-	int bpp;
-	int size_l;
-	int endiant;
-
 	
-	data = malloc(sizeof (data_t));
+	data = malloc(sizeof(data_t));
    if ((data->mlx_ptr = mlx_init()) == NULL)
         return (EXIT_FAILURE);
 	if ((data->win_ptr = mlx_new_window(data->mlx_ptr, 640, 480, "marche pas")) == NULL)
         return (EXIT_FAILURE);
 	if ((data->img_ptr = mlx_new_image (data->mlx_ptr, 640, 480)) == NULL)
         return (EXIT_FAILURE);
-	mlx_pixel_put (data->mlx_ptr, data->win_ptr, 300, 300, 16711680);
-	if ((data->addr_ptr = mlx_get_data_addr (data->img_ptr, &bpp, &size_l, &endiant)) == NULL)
+	if ((data->addr_ptr = mlx_get_data_addr (data->img_ptr, &data->bpp, &data->size_l, &data->endiant)) == NULL)
         return (EXIT_FAILURE);
-	printf("|%d|\n", bpp);
-	printf("|%d|\n", size_l);
-	printf("|%d|\n", endiant);
-	*((int *)(data->addr_ptr + (50 + 100*480 / bpp/8))) = 427819008;
-	mlx_put_image_to_window (data->mlx_ptr, data->win_ptr, data->img_ptr, 640, 480);
+	data->bpp /= 8;
+	*(int *)(data->addr_ptr + ((50 + 100 * 480) * data->bpp)) = 16711680;
+	mlx_put_image_to_window (data->mlx_ptr, data->win_ptr, data->img_ptr, 0, 0);
 	mlx_loop(data->mlx_ptr);
     return (EXIT_SUCCESS);
 }
