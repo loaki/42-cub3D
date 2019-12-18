f52bbef75fec76e376085fb7eccecfaf9afa7cb9
loaki <jules.feuillatre@gmail.com>
2019-12-18 16:39:31 +0100

    ok

--- ft_size_wall.c
+++ ft_size_wall.c
@@ -6,7 +6,7 @@
 /*   By: jfeuilla <jfeuilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 14:06:41 by jfeuilla          #+#    #+#             */
-/*   Updated: 2019/12/18 16:33:23 by jfeuilla         ###   ########.fr       */
+/*   Updated: 2019/12/18 16:39:20 by jfeuilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ double	ft_size_wall_xp(t_data *data, t_pos *pos, double x, double y)
 			x = (int)(pos->x) + i;
 			y = pos->y + fabs(x - pos->x) / pos->vector_x_mod * pos->vector_y_mod;
 		}
-		if ((int)y <= data->height && data->map[(int)y][(int)x] != '0')
+		if ((int)y <= data->height && data->map[(int)x][(int)y] != '0')
 		{
 			return (data->height / (sqrt((pos->x - x) * (pos->x - x) +
 			(pos->y - y) * (pos->y - y))));
@@ -56,7 +56,7 @@ double	ft_size_wall_xn(t_data *data, t_pos *pos, double x, double y)
 			x = (int)(pos->x) - i;
 			y = pos->y + fabs(x - pos->x) / pos->vector_x_mod * pos->vector_y_mod;
 		}
-		if ((int)y <= data->height && data->map[(int)y][(int)x] != '0')
+		if ((int)y <= data->height && data->map[(int)x][(int)y] != '0')
 		{
 			return (data->height / (sqrt((pos->x - x) * (pos->x - x) +
 			(pos->y - y) * (pos->y - y))));
@@ -83,7 +83,7 @@ double	ft_size_wall_yp(t_data *data, t_pos *pos, double x, double y)
 			y = (int)(pos->y) + i;
 			x = pos->x + fabs(y - pos->y) / pos->vector_y_mod * pos->vector_x_mod;
 		}
-		if ((int)x <= data->width && data->map[(int)y][(int)x] != '0')
+		if ((int)x <= data->width && data->map[(int)x][(int)y] != '0')
 		{
 			return (data->height / (sqrt((pos->x - x) * (pos->x - x) +
 			(pos->y - y) * (pos->y - y))));
@@ -110,7 +110,7 @@ double	ft_size_wall_yn(t_data *data, t_pos *pos, double x, double y)
 			y = (int)(pos->y) - i;
 			x = pos->x + fabs(y - pos->y) / pos->vector_y_mod * pos->vector_x_mod;
 		}
-		if ((int)x <= data->width && data->map[(int)y][(int)x] != '0')
+		if ((int)x <= data->width && data->map[(int)x][(int)y] != '0')
 		{
 			return (data->height / (sqrt((pos->x - x) * (pos->x - x) +
 			(pos->y - y) * (pos->y - y))));
