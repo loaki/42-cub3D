8a23a63ee546cc1ff19405f4fc56955c1471109b
loaki <jules.feuillatre@gmail.com>
2020-01-11 15:30:15 +0100

    save

--- cub3D/ft_move.c
+++ cub3D/ft_move.c
@@ -6,31 +6,24 @@
 /*   By: jfeuilla <jfeuilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 12:20:47 by jfeuilla          #+#    #+#             */
-/*   Updated: 2020/01/09 16:16:03 by jfeuilla         ###   ########.fr       */
+/*   Updated: 2020/01/11 15:03:16 by jfeuilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int		ft_collision(t_data *data)
-{
-	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
-		data->pos_y < 0 || data->pos_x < 0)
-		return (1);
-	if (data->map[(int)data->pos_y][(int)data->pos_x] == '1')
-		return (1);
-	return (0);
-}
-
 int		ft_move_f(t_data *data)
 {
 	data->pos_x += 0.1 * data->vector_x;
-	data->pos_y -= 0.1 * data->vector_y;
-	if (ft_collision(data) == 1)
-	{
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 || 
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_x -= 0.1 * data->vector_x;
+	data->pos_y -= 0.1 * data->vector_y;
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_y += 0.1 * data->vector_y;
-	}
 	ft_info(data);
 	return (ft_view(data));
 }
@@ -38,12 +31,15 @@ int		ft_move_f(t_data *data)
 int		ft_move_b(t_data *data)
 {
 	data->pos_x -= 0.1 * data->vector_x;
-	data->pos_y += 0.1 * data->vector_y;
-	if (ft_collision(data) == 1)
-	{
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_x += 0.1 * data->vector_x;
+	data->pos_y += 0.1 * data->vector_y;
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_y -= 0.1 * data->vector_y;
-	}
 	ft_info(data);
 	return (ft_view(data));
 }
@@ -51,12 +47,15 @@ int		ft_move_b(t_data *data)
 int		ft_move_r(t_data *data)
 {
 	data->pos_x += 0.1 * data->vector_y;
-	data->pos_y += 0.1 * data->vector_x;
-	if (ft_collision(data) == 1)
-	{
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_x -= 0.1 * data->vector_y;
+	data->pos_y += 0.1 * data->vector_x;
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_y -= 0.1 * data->vector_x;
-	}
 	ft_info(data);
 	return (ft_view(data));
 }
@@ -64,12 +63,15 @@ int		ft_move_r(t_data *data)
 int		ft_move_l(t_data *data)
 {
 	data->pos_x -= 0.1 * data->vector_y;
-	data->pos_y -= 0.1 * data->vector_x;
-	if (ft_collision(data) == 1)
-	{
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_x += 0.1 * data->vector_y;
+	data->pos_y -= 0.1 * data->vector_x;
+	if (data->pos_y > (data->height - 1) || data->pos_x > (data->width - 1) ||
+		data->pos_y < 0 || data->pos_x < 0 ||
+		data->map[(int)data->pos_y][(int)data->pos_x] == '1')
 		data->pos_y += 0.1 * data->vector_x;
-	}
 	ft_info(data);
 	return (ft_view(data));
 }
--- cub3D/ft_size_wall.c
+++ cub3D/ft_size_wall.c
@@ -6,7 +6,7 @@
 /*   By: jfeuilla <jfeuilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 14:06:41 by jfeuilla          #+#    #+#             */
-/*   Updated: 2020/01/09 17:34:52 by jfeuilla         ###   ########.fr       */
+/*   Updated: 2020/01/11 15:28:36 by jfeuilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ double	ft_size_wall_xn(t_data *data, double x, double y)
 	i = 0;
 	while (i < data->width || i < data->height)
 	{
-		x = (int)ceil(data->pos_x) - i;
+		x = (int)(data->pos_x) - i;
 		y = data->pos_y + fabs(x - (double)data->pos_x) / data->vector_x_mod *
 		data->vector_y_mod;
 		if ((int)(x) <= data->width && (int)(y) <=
-			(data->height - 1) && (int)(x) >= 1 && (int)(y) >= 0 && x != ceil(data->pos_x) &&
+			(data->height - 1) && (int)(x) >= 1 && (int)(y) >= 0 &&
 			data->map[(int)(y)][(int)(x - 1)] == '1')
 			return ((double)data->res_y / (sqrtf(((double)data->pos_x - x) *
 			((double)data->pos_x - x) + ((double)data->pos_y - y) *
@@ -61,11 +61,11 @@ double	ft_size_wall_yp(t_data *data, double x, double y)
 	i = 0;
 	while (i < data->width || i < data->height)
 	{
-		y = (int)ceil(data->pos_y) - i;
+		y = (int)(data->pos_y) - i;
 		x = data->pos_x + fabs(y - (double)data->pos_y) / data->vector_y_mod *
 		data->vector_x_mod;
 		if ((int)(x) <= (data->width - 1) && (int)(y) <=
-			data->height && (int)(x) >= 0 && (int)(y) >= 1 && y != ceil(data->pos_y) &&
+			data->height && (int)(x) >= 0 && (int)(y) >= 1 &&
 			data->map[(int)(y - 1)][(int)(x)] == '1')
 			return ((double)data->res_y / (sqrtf(((double)data->pos_x - x) *
 			((double)data->pos_x - x) + ((double)data->pos_y - y) *
